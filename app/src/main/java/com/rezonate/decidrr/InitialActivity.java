4c93121ebfec4225dc2e616ed9c8fe78abb41926
jpinz <julian.pinzer@gmail.com>
2015-04-12 21:20:53 -0400

    No need to log in every time the app is launched.

--- app/src/main/java/com/rezonate/decidrr/InitialActivity.java
+++ app/src/main/java/com/rezonate/decidrr/InitialActivity.java
@@ -1,92 +1,50 @@
 package com.rezonate.decidrr;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
 import android.content.Intent;
-import android.view.View;
+import android.os.Bundle;
 
 import com.crashlytics.android.Crashlytics;
 import com.mopub.common.MoPub;
 import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.Callback;
-import com.twitter.sdk.android.core.Result;
+
+import com.digits.sdk.android.Digits;
+
+
+import com.twitter.sdk.android.core.Session;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.core.TwitterException;
-import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
 import io.fabric.sdk.android.Fabric;
 
 
-public class InitialActivity extends ActionBarActivity {
+public class InitialActivity extends Activity {
 
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
     private static final String TWITTER_KEY = "x3NWZS629e9WrUtysgVE7xYgm";
     private static final String TWITTER_SECRET = "8F0ILemevPTea8fDCUExH7n437F9TDIP82EhHjTGHOkVbEn3oq";
-
-    private boolean loggedIn = false;
-
-    private TwitterLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig), new Crashlytics(), new MoPub());
-        if (loggedIn == true){skipLogin(null);}
-        else {
-            setContentView(R.layout.activity_initial);
-            loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
-            loginButton.setCallback(new Callback<TwitterSession>() {
-                @Override
-                public void success(Result<TwitterSession> result) {
-                    loggedIn = true;
-                }
+        final Session activeSession = SessionRecorder.recordInitialSessionState(
+                Twitter.getSessionManager().getActiveSession(),
+                Digits.getSessionManager().getActiveSession()
+        );
 
-                @Override
-                public void failure(TwitterException exception) {
-                    // Do something on failure
-                    loggedIn = false;
-                }
-            });
+        if (activeSession != null) {
+            startThemeActivity();
+        } else {
+            startLoginActivity();
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        loginButton.onActivityResult(requestCode, resultCode, data);
-        loggedIn = true;
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+    private void startThemeActivity() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_initial, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void skipLogin(View view) {
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-        loggedIn = true;
+    private void startLoginActivity() {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
